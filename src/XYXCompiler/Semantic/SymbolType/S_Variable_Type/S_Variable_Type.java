0f6d674f96e5d39fd46fae185c75a3d08b6e8d80
woshiyyya <1085966850@qq.com>
2018-04-19 12:21:21 +0800

    clear

--- src/XYXCompiler/Semantic/SymbolType/S_Variable_Type/S_Variable_Type.java
+++
@@ -1,7 +0,0 @@
-package XYXCompiler.Semantic.SymbolType.S_Variable_Type;
-
-import XYXCompiler.Semantic.SymbolType.S_Type;
-
-public abstract class S_Variable_Type extends S_Type {
-
-}
--- src/XYXCompiler/Tools/Exceptions/XYXException.java
+++
@@ -1,7 +0,0 @@
-package XYXCompiler.Tools.Exceptions;
-
-public class XYXException extends Exception {
-    public XYXException (String info){
-        super(info);
-    }
-}
